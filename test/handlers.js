31ab46c928058f22145f8d834c3a21dece6dae5e
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2018-05-20 19:48:32 +0300

    Refactor BitBucket web hook

--- test/handlers.js
+++ test/handlers.js
@@ -5,7 +5,6 @@ var handlers = require('../lib/handlers');
 describe('handlers.js', function() {
   it('should return pushed changes to repository message', function () {
     var request = {
-      body: {
         push: {
           changes: [{
             old: {
@@ -39,7 +38,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleRepoPush(request);
     expect(message).to.be.equal("*John Dow* pushed changes into branch [master](http://repository1.com/master) in repository [Repository 1](http://repository1.com)");
@@ -47,7 +45,6 @@ describe('handlers.js', function() {
 
   it('should return no message for duplicated repo:push event', function () {
     var request = {
-      body: {
         push: {
           changes: [{
             old: {
@@ -72,7 +69,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleRepoPush(request);
     expect(message).to.be.null;
@@ -80,7 +76,6 @@ describe('handlers.js', function() {
 
   it('should return issue created message', function () {
     var request = {
-      body: {
         issue: {
           id: 1,
           title: "Issue 1",
@@ -93,7 +88,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleIssueCreated(request);
     expect(message).to.be.equal("*John Dow* created a new issue [Issue 1](http://repository1.com/issues/1)");
@@ -101,7 +95,6 @@ describe('handlers.js', function() {
 
   it('should return issue updated message', function () {
     var request = {
-      body: {
         issue: {
           id: 1,
           title: "Issue 1",
@@ -114,7 +107,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleIssueUpdated(request);
     expect(message).to.be.equal("*John Dow* updated issue [Issue 1](http://repository1.com/issues/1)");
@@ -122,7 +114,6 @@ describe('handlers.js', function() {
 
   it('should return issue resolved message', function () {
     var request = {
-      body: {
         issue: {
           id: 1,
           title: "Issue 1",
@@ -140,7 +131,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleIssueUpdated(request);
     expect(message).to.be.equal("*John Dow* resolved issue [Issue 1](http://repository1.com/issues/1)");
@@ -148,7 +138,6 @@ describe('handlers.js', function() {
 
   it('should return issue comment created message', function () {
     var request = {
-      body: {
         issue: {
           id: 1,
           title: "Issue 1",
@@ -169,7 +158,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handleIssueCommentCreated(request);
     expect(message).to.be.equal("*John Dow* added a new comment to the issue [Issue 1](http://repository1.com/issues/1#comment-1)");
@@ -177,7 +165,6 @@ describe('handlers.js', function() {
 
   it('should return pull request approved message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -190,7 +177,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestApproved(request);
     expect(message).to.be.equal("*John Dow* approved pull request [Pull Request 1](http://repository1.com/pull-requests/1)");
@@ -198,7 +184,6 @@ describe('handlers.js', function() {
 
   it('should return pull request created message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -211,7 +196,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestCreated(request);
     expect(message).to.be.equal("*John Dow* created a new pull request [Pull Request 1](http://repository1.com/pull-requests/1)");
@@ -219,7 +203,6 @@ describe('handlers.js', function() {
 
   it('should return pull request updated message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -232,7 +215,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestUpdated(request);
     expect(message).to.be.equal("*John Dow* updated pull request [Pull Request 1](http://repository1.com/pull-requests/1)");
@@ -240,7 +222,6 @@ describe('handlers.js', function() {
 
   it('should return pull request comment created message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -261,7 +242,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestCommentCreated(request);
     expect(message).to.be.equal("*John Dow* commented on a pull request [Pull Request 1](http://repository1.com/pull-requests/1#comment-1)");
@@ -269,7 +249,6 @@ describe('handlers.js', function() {
 
   it('should return pull request comment updated message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -290,7 +269,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestCommentUpdated(request);
     expect(message).to.be.equal("*John Dow* updated comment on a pull request [Pull Request 1](http://repository1.com/pull-requests/1#comment-1)");
@@ -298,7 +276,6 @@ describe('handlers.js', function() {
 
   it('should return pull request merged message', function () {
     var request = {
-      body: {
         pullrequest: {
           id: 1,
           title: "Pull Request 1",
@@ -319,7 +296,6 @@ describe('handlers.js', function() {
         actor: {
           display_name: "John Dow"
         }
-      }
     };
     var message = handlers.handlePullRequestMerged(request);
     expect(message).to.be.equal("*John Dow* merged pull request [Pull Request 1](http://repository1.com/pull-requests/1)");
